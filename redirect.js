a431d99ca2df3d718a21e4c42970c168ff98dc67
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2022-12-27 17:34:12 -0500

    Update

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 function checkbrowser() {
     console.log(navigator.userAgent.indexOf("Safari"))
-    if (navigator.userAgent.indexOf("Safari") === 81) {
+    if (navigator.userAgent.indexOf("Safari") === -1) {
     document.location.href = "https://xn--98h.madebyesmel.com/"
 }
 };
