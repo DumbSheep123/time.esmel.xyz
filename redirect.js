a2c4b090877a4f8fc961b0312679288489a7299f
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2022-12-27 18:18:24 -0500

    code that works, yaya

--- redirect.js
+++ redirect.js
@@ -1,12 +1,3 @@
-//let safariAgent = userAgentString.indexOf("Safari") > -1;
-
-/*window.onload = function() {
-    console.log(navigator.userAgent.indexOf("Safari"))
-    if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
-        document.location.href = "https://xn--9x8h.madebyesmel.com"
-    }
-};*/
-
 if (/Safari|iPad|iPhone|iPhone/i.test(navigator.userAgent)) {
     document.location = "https://xn--9x8h.madebyesmel.com"
   }
