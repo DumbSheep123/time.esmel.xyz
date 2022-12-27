be3371378038a519d71f0c61361ac07d9d5f2563
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2022-12-27 17:47:24 -0500

    w

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     console.log(navigator.userAgent.indexOf("Safari"))
-    if (navigator.userAgent.indexOf("Safari") === -1) {
+    if (navigator.userAgent.indexOf("Safari") = -1) {
         document.location.href = "https://xn--9x8h.madebyesmel.com"
     }
 };
