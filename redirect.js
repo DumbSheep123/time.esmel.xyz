feea2ef615892e0a03ef48d39252fc314ed1f64c
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2022-12-27 19:23:28 -0500

    d

--- redirect.js
+++ redirect.js
@@ -1,3 +1,4 @@
-if (/Safari|iPad|iPhone|Apple|iPhone/i.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent)) {
+if (/(Safari)|(iPad)|(iPhone)|(Apple)|(iPhone)|(iPod)/i.test(navigator.userAgent)
+    && !/Chrome/.test(navigator.userAgent)) {
     window.location.href = "https://xn--9x8h.madebyesmel.com"
 }
