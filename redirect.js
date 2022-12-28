55077414dac36a702daed28ad44b1a83935ec411
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2022-12-27 19:09:23 -0500

    idk tbh

--- redirect.js
+++ redirect.js
@@ -1,3 +1,3 @@
-if (/Safari|iPad|iPhone|iPhone/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent)) {
+if (/Safari|iPad|iPhone|iPhone/i.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent)) {
     window.location.href = "https://xn--9x8h.madebyesmel.com"
 }
