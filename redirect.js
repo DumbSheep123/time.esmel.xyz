3374d9b58e51368f41ddedfff313ed580d8c95d7
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2022-12-28 18:10:49 -0500

    d

--- redirect.js
+++ redirect.js
@@ -1,4 +1,10 @@
-if (/(Safari)|(iPad)|(iPhone)|(Apple)|(iPhone)|(iPod)/i.test(navigator.userAgent)
+/*if (/(Safari)|(iPad)|(iPhone)|(Apple)|(iPhone)|(iPod)/i.test(navigator.userAgent)
     && !/Chrome/.test(navigator.userAgent)) {
     window.location.href = "https://xn--9x8h.madebyesmel.com"
-}
+}*/
+
+window.onload = function redirectSafari() {
+    if (navigator.userAgent.indexOf('Safari', 'iPhone') !== -1 && navigator.userAgent.indexOf('Chrome') === -1) {
+      window.location.replace("https://www.example.com/safari-redirect");
+    }
+  }
