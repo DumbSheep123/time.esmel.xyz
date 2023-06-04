018fb9dc693f5077b963e2c13f1fe1968d9a9029
Andrew Frykman <andrewfrykman123@icloud.com>
2023-06-04 09:46:08 -0400

    joe

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-const date = document.getElementById("date");
-const time = document.getElementById("time");
-
-const week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+const [date, time, week] = [document.getElementById("date"), document.getElementById("time"), ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]];
 
 function updateTime() {
-    const cd = new Date();
-    const currentDate = `${cd.getFullYear()}-${zeroPadding(cd.getMonth() + 1, 2)}-${zeroPadding(cd.getDate(), 2)} ${week[cd.getDay()]}`;
-    const currentTime = `${zeroPadding(cd.getHours(), 2)}:${zeroPadding(cd.getMinutes(), 2)}:${zeroPadding(cd.getSeconds(), 2)}`;
-    date.textContent = currentDate;
-    time.textContent = currentTime;
+  const d = new Date();
+  date.textContent = `${d.getFullYear()}-${String(d.getMonth()+1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')} ${week[d.getDay()]}`;
+  time.textContent = `${String(d.getHours()).padStart(2, '0')}:${String(d.getMinutes()).padStart(2, '0')}:${String(d.getSeconds()).padStart(2, '0')}`;
 }
 
 setInterval(updateTime, 100);
-
-function zeroPadding(num, digit) {
-    return String(num).padStart(digit, '0');
-}
-
 setInterval(() => document.title = `${time.textContent} ${date.textContent}`, 10);
